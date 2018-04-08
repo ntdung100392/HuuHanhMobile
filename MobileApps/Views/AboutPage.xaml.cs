0ed21eca9263ae2e6360004016d75dc659f4ced0
Dũng Nguyễn Trung <dung.nt100392@gmail.com>
2018-04-08 16:42:34 +0700

    Init apps

---
+++ MobileApps/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace MobileApps
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
